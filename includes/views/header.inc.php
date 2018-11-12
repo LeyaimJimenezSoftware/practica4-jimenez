0c53d766de8a4f53980071a47fa8789f099e79c1
LeyaimJimenezSoftware <ljimenez6@ucol.mx>
2018-11-11 18:31:26 -0600

    P4 primera parte

---
+++ includes/views/header.inc.php
@@ -0,0 +1,59 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Simple cat&aacute;logo </title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <link rel="manifest" href="./js/manifest.json">
+    <meta name="theme-color" content="#F7DF1E">
+    
+    <!-- Detección de Icono de PWA -->
+    <link rel="icon" type="image/png" sizes="16x16" href="./img/icon_16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./img/icon_32x32.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="./img/icon_64x64.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="./img/icon_96x96.png">
+    <link rel="icon" type="image/png" sizes="128x128" href="./img/icon_128x128.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="./img/icon_192x192.png">
+    <link rel="icon" type="image/png" sizes="256x256" href="./img/icon_256x256.png">
+    <link rel="icon" type="image/png" sizes="384x384" href="./img/icon_384x384.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="./img/icon_512x512.png">
+    <link rel="icon" type="image/png" sizes="1024x1024" href="./img/icon_1024x1024.png">
+    <!-- Metatags iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-title" content="PWA Demo">
+    <link rel="apple-touch-icon" sizes="192x192" href="./img/icon_192x192.png">
+    <!-- Metatags Windows -->
+    <meta name="msapplication-TileColor" content="#F7DF1E">
+    <meta name="msapplication-TileImage" content="./img/icon_192x192.png">
+    <!-- Otros Metatags -->
+    <meta property="og:title" content="PWA Demo">
+    <meta property="og:locale" content="es_MX">
+    <meta property="og:type" content="website">
+    <meta property="og:image" content="./img/icon_128x128.png">
+    <meta property="og:site_name" content="EDteam">
+    <meta property="og:url" content="https://pwa.ed.team">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+	
+<body class="page">
+<header>
+    <nav role="navigation">
+        <div class="container">
+            <div class="page-header">
+                <h1>Direcci&oacute;n de sistemas <small>Cat&aacute;logo de personas</small></h1>
+            </div>
+        </div>
+    </nav>        
+</header>
+<hr />
+<!-- end of header -->
+
+
+	<!-- div class="container">
+
+		<h1>Simple Blog - MySqli</h1>
+		<h3><a href="create.php">Add Post</a></h3><hr / -->
